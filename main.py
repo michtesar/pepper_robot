0ed8ea60d5127c13d973e3c367596731736ab401
Michael Tesař <michael.tesar@nudz.cz>
2018-05-10 14:07:25 +0100

    Add some concept of the app

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
 from robot import *
+import time
 
-pepper = Pepper("paprika.local")
+pepper = Pepper("pepper.local")
 
-#pepper.play_sound("/home/nao/song.mp3")
-pepper.stop_sound()
+#pepper.stat_animation("Hey_1")
 
-#pepper.share_localhost("tmp/")
-#pepper.load_map("2018-05-04T154748.673Z.explo")
-#pepper.show_map(on_robot=True, remote_ip="192.168.0.102")
 
-#import time
-#time.sleep(10)
-#pepper.pick_a_volunteer()
+"""
+print("[PIPELINE]: Setting the robot")
+pepper.set_volume(30)
+pepper.set_security_distance(0.01)
+pepper.tablet_show_web("https://www.ciirc.cvut.cz/cs/")
+
+print("[PIPELINE]: Start exploration")
+pepper.exploration_mode(1)
+pepper.robot_localization()
+pepper.show_map(on_robot=True, remote_ip="192.168.0.103")
+
+print("[PIPELINE]: Navigate to 1.0")
+pepper.navigate_to(1.0, 0.0)
+pepper.robot_localization()
+
+print("[PIPELINE]: Navigate to 2.0")
+pepper.navigate_to(2.0, 0.0)
+pepper.robot_localization()
+
+print("[PIPELINE]: Navigate to -1.0")
+pepper.navigate_to(-1.0, 0.0)
+pepper.robot_localization()
+
+print("[PIPELINE]: Navigate to -2.0")
+pepper.navigate_to(-2.0, 0.0)
+pepper.robot_localization()
+
+print("[PIPELINE]: Pick a volunteer")
+pepper.pick_a_volunteer()
+
+print("[PIPELINE]: Play sound")
+pepper.play_sound("/home/nao/song.mp3")
+time.sleep(20)
+
+# TODO: Face recognition - age - sex - mood detection
+# TODO: Party mode on
+"""

5e88a27bf57e9ce0105040ea817f1d0b534f79b8
Michael Tesař <michael.tesar@nudz.cz>
2018-05-11 19:05:53 +0100

    Basic idea of the task

--- main.py
+++ main.py
@@ -2,32 +2,20 @@ from robot import *
 import time
 
 pepper = Pepper("paprika.local")
-pepper.get_face_properties()
 
-#pepper.start_animation("Hey_1")
-
-
-"""
 print("[PIPELINE]: Setting the robot")
-pepper.set_volume(30)
+pepper.set_volume(50)
 pepper.set_security_distance(0.01)
 pepper.tablet_show_web("https://www.ciirc.cvut.cz/cs/")
+pepper.start_animation("Hey_1")
 
 print("[PIPELINE]: Start exploration")
-pepper.exploration_mode(1)
+pepper.exploration_mode(2)
 pepper.robot_localization()
 pepper.show_map(on_robot=True, remote_ip="192.168.0.103")
 
-print("[PIPELINE]: Navigate to 1.0")
-pepper.navigate_to(1.0, 0.0)
-pepper.robot_localization()
-
 print("[PIPELINE]: Navigate to 2.0")
 pepper.navigate_to(2.0, 0.0)
-pepper.robot_localization()
-
-print("[PIPELINE]: Navigate to -1.0")
-pepper.navigate_to(-1.0, 0.0)
 pepper.robot_localization()
 
 print("[PIPELINE]: Navigate to -2.0")
@@ -36,11 +24,10 @@ pepper.robot_localization()
 
 print("[PIPELINE]: Pick a volunteer")
 pepper.pick_a_volunteer()
+pepper.get_face_properties()
 
 print("[PIPELINE]: Play sound")
+pepper.start_dance()
 pepper.play_sound("/home/nao/song.mp3")
 time.sleep(20)
-
-# TODO: Face recognition - age - sex - mood detection
-# TODO: Party mode on
-"""
+pepper.stop_sound()

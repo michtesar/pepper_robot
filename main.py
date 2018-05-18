5dd6c9922b268602e09df7076044e1c19fc5be71
Michael Tesař <michael.tesar@nudz.cz>
2018-05-18 14:42:28 +0200

    Basic demo setup

--- main.py
+++ main.py
@@ -1,33 +1,14 @@
 from robot import *
-import cv2
 import time
 
 pepper = Pepper("10.37.1.227")
 
-pepper.subscribe_camera("camera_depth", 3, 30)
-pepper.get_depth_frame(True)
-pepper.unsubscribe_camera()
-
-
-"""
 print("[PIPELINE]: Setting the robot")
+pepper.set_awareness(False)
 pepper.set_volume(50)
 pepper.set_security_distance(0.01)
 pepper.tablet_show_web("https://www.ciirc.cvut.cz/cs/")
 pepper.start_animation("Hey_1")
-
-print("[PIPELINE]: Start exploration")
-pepper.exploration_mode(2)
-pepper.robot_localization()
-pepper.show_map(on_robot=True, remote_ip="192.168.0.103")
-
-print("[PIPELINE]: Navigate to 2.0")
-pepper.navigate_to(2.0, 0.0)
-pepper.robot_localization()
-
-print("[PIPELINE]: Navigate to -2.0")
-pepper.navigate_to(-2.0, 0.0)
-pepper.robot_localization()
 
 print("[PIPELINE]: Pick a volunteer")
 pepper.pick_a_volunteer()
@@ -35,8 +16,7 @@ pepper.get_face_properties()
 
 print("[PIPELINE]: Play sound")
 pepper.tablet_show_image("https://goo.gl/4Xq6Bc")
-pepper.start_dance()
 pepper.play_sound("/home/nao/song.mp3")
+pepper.start_dance()
 time.sleep(20)
 pepper.stop_sound()
-"""
